e4097ca8edcbbb247731ce852b5640ea2929c1e1
Benjamin Manns <benmanns@gmail.com>
2014-02-16 18:52:25 -0800

    Instrument slow worker methods.

--- app/workers/slow_worker.rb
+++ app/workers/slow_worker.rb
@@ -1,5 +1,9 @@
+require "new_relic/agent/method_tracer"
+
 class SlowWorker
   @queue = :slow
+
+  include ::NewRelic::Agent::MethodTracer
 
   def self.perform
     big
@@ -19,5 +23,11 @@ class SlowWorker
 
   def self.big
     sleep 10
+  end
+
+  class << self
+    add_method_tracer :small
+    add_method_tracer :medium
+    add_method_tracer :big
   end
 end
